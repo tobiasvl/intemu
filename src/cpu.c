5bc63cfaf68ee8ec69ee6fb6cbbf481c694ebe63
Axel Sanner <axl@uio.no>
2024-10-11 11:25:37 +0200

    moved something

--- src/cpu.c
+++ src/cpu.c
@@ -7,31 +7,16 @@
 #include "cpu.h"
 #include "bus.h"
 
-//  Memset bus memory
-//  cpu memset 0 
-//  set pc til 0x100
-//  lag read from file
-//  lag fetch byte
+//  DONE - Memset bus memory
+//  DONE - cpu memset 0 
+//  DONE - set pc til 0x100
+//  DONE - lag read from file
+//  DONE - lag fetch byte
 //  lag fetch uint16
 //  lag decoder uint8
 
 int decode_opcode(uint8_t opcode)
 {
-    switch (opcode) {
-        case 0x000:
-            //fprintf(stderr, "0x0000 ");
-            break;
-        case 0x001:
-            fprintf(stderr, "0x0001 ");
-            // FOO
-            break;
-        case 0x002:
-            fprintf(stderr, "0x0002 ");
-            // BAR
-            break;
-        default:
-            // code block
-    }
 
     return 0;
 
@@ -55,7 +40,22 @@ int fetch(S_CPU *cpu, S_BUS *bus)
     while (true)
     {
         uint8_t byte = bus->memory[cpu->PC];
-        decode_opcode(byte);
+        //decode_opcode(byte);
+        switch (byte) {
+            case 0x000:
+                //fprintf(stderr, "0x0000 ");
+                break;
+            case 0x001:
+                fprintf(stderr, "0x0001 ");
+                // FOO
+                break;
+            case 0x002:
+                fprintf(stderr, "0x0002 ");
+                // BAR
+                break;
+            default:
+                // code block
+        }
 
         //Need an exit path
         // if something then exit
